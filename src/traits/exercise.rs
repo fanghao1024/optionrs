709240e75f68e502c0383d50df54c445dd6f5e17
hao fang <chinafanghao@foxmail.com>
2025-12-17 13:58:04 +0800

    Update exercise.rs

---
+++ src/traits/exercise.rs
@@ -0,0 +1,46 @@
+/// define the interface for exercise rules
+/// 定义行权规则接口
+pub trait ExerciseRule{
+    /// Determine whether to exercise the right at a given time point
+    ///
+    /// 判断在给定时点是否应行权
+    /// # parameters
+    /// + time: time until expiration(in year) 距离到期的时间（年）
+    /// + spot: current underlying asset price 当前标的资产价格
+    /// + intrinsic_value: intrisic value 内在价值
+    /// + continuation_value: continue holding value 继续持有价值
+    fn should_exercise(
+        &self,
+        time:f64,
+        spot:f64,
+        intrinsic_value:f64,
+        continuation_value:f64
+    ) ->bool;
+}
+
+/// European exercise rule
+/// 欧式行权规则
+#[derive(Debug,Clone,Copy)]
+pub struct EuropeanExercise;
+
+impl ExerciseRule for EuropeanExercise{
+    fn should_exercise(
+        &self,
+        time: f64,
+        spot: f64,
+        intrinsic_value: f64,
+        continuation_value: f64
+    ) -> bool {
+        time<1e-9
+    }
+}
+
+/// American exercise rule
+/// 美式行权规则
+pub struct AmericanExercise;
+
+impl ExerciseRule for AmericanExercise{
+    fn should_exercise(&self, time: f64, spot: f64, intrinsic_value: f64, continuation_value: f64) -> bool {
+        intrinsic_value>continuation_value
+    }
+}
